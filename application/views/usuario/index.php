8d62d6d258ef275fc766421d96f6158098d67992
yordy <juanyordyguillen@gmail.com>
2017-05-10 23:21:39 -0500

    actualizo index

--- application/views/usuario/index.php
+++ application/views/usuario/index.php
@@ -5,7 +5,7 @@
 
   <!-- Nav tabs -->
   <ul class="nav nav-tabs" role="tablist">
-    <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">CONSULTA</a></li>
+    <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">CONSULTA usuario</a></li>
     <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">REGISTRO</a></li>
   </ul>
 
